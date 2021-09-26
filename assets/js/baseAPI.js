bf32e287d7f5b3a6134cc85219c4c281f3c10d7a
DJ <2587572166@qq.com>
2021-09-26 15:33:29 +0800

    完成了主页功能和显示

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,15 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        };
+    }
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message == "身份认证失败！") {
+            localStorage.removeItem('token');
+            location.href = "http://127.0.0.1:5500/%E9%98%B6%E6%AE%B5%E5%9B%9B/%E7%AC%AC%E5%85%AB%E7%AB%A0%E5%A4%A7%E4%BA%8B%E4%BB%B6%E5%90%8E%E5%8F%B0%E7%AE%A1%E7%90%86%E9%A1%B9%E7%9B%AE/background/login.html";
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+$(function () {
+    getUserInfo();
+    var layer = layui.layer;
+    $("#btnLogout").on('click', function () {
+        layer.confirm('你真的想退出吗?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //do something
+            localStorage.removeItem("token");
+            location.href = "http://127.0.0.1:5500/%E9%98%B6%E6%AE%B5%E5%9B%9B/%E7%AC%AC%E5%85%AB%E7%AB%A0%E5%A4%A7%E4%BA%8B%E4%BB%B6%E5%90%8E%E5%8F%B0%E7%AE%A1%E7%90%86%E9%A1%B9%E7%9B%AE/background/login.html";
+            layer.close(index);
+        });
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: "/my/userinfo",
+        // headers 就是请求头配置对象
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            console.log(res);
+            renderAvatar(res.data);
+        },
+        /*         complete: function (res) {
+                    if (res.responseJSON.status === 1 && res.responseJSON.message == "身份认证失败！") {
+                        localStorage.removeItem('token');
+                        location.href = "http://127.0.0.1:5500/%E9%98%B6%E6%AE%B5%E5%9B%9B/%E7%AC%AC%E5%85%AB%E7%AB%A0%E5%A4%A7%E4%BA%8B%E4%BB%B6%E5%90%8E%E5%8F%B0%E7%AE%A1%E7%90%86%E9%A1%B9%E7%9B%AE/background/login.html";
+
+                    }
+                } */
+
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $(".welcome").html("你好&nbsp;&nbsp;" + name);
+
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr("src", user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first)
+            .show();
+    }
+}
